67615e464a83d23c3ce57376c4219ab53b1669c8
Sebas Rossi <sebashrossi@gmail.com>
2021-03-05 12:22:40 +0100

    uploading image completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = JSON.parse(open(url).read)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = JSON.parse(open(url).read)
 
-puts 'Destroying ingredients...'
-Ingredient.destroy_all
+# puts 'Destroying ingredients...'
+# Ingredient.destroy_all
 
-ingredients['drinks'].each do |drink|
-  ingr = Ingredient.create(name: drink['strIngredient1'])
-  puts "Creating ingredient...#{drink['strIngredient1']}"
+# ingredients['drinks'].each do |drink|
+#   ingr = Ingredient.create(name: drink['strIngredient1'])
+#   puts "Creating ingredient...#{drink['strIngredient1']}"
+# end
+puts 'Destroying cocktails...'
+Cocktail.destroy_all
+
+file = URI.open('https://www.umami.site/wp-content/uploads/2016/03/rum-coke-2.jpg')
+5.times do
+cocktail = Cocktail.new(name: 'Rum&Coke')
+cocktail.photo.attach(io: file, filename: 'rumcoke.png', content_type: 'image/png')
 end
-
 puts 'Completed...'
-
